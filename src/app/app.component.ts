96e90f1bb9dabc071ae8391e128cc5b13b808345
martinpaezi <paezmartin99@gmail.com>
2024-03-27 15:16:32 -0300

    Lista de alumnos con condicion de aprobado y desaprobado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practica2';
+
+  mostrarLista = true;
+
+  estudiantes = [
+    {
+      nombre: "Obi-wan",
+      nota:8,
+      aprobado: true,
+  },
+  {
+    nombre:"Ahsoka Tano",
+    nota: 5,
+    aprobado: false,
+  },
+  {
+    nombre: "Anakin",
+    nota:4,
+    aprobado: false,
+  },
+  {
+    nombre: "Yoda",
+    nota: 10,
+    aprobado: true,
+  }
+  ]
 }
