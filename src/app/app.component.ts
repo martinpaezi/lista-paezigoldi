430450850c381124c3edb64dd7f3e268e084c663
martinpaezi <paezmartin99@gmail.com>
2024-03-27 17:00:27 -0300

    seccion regular y promocion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,21 +15,61 @@ export class AppComponent {
       nombre: "Obi-wan",
       nota:8,
       aprobado: true,
+      libre: false,
+      promocion: true,
   },
   {
     nombre:"Ahsoka Tano",
     nota: 5,
     aprobado: false,
+    libre: true,
+    promocion: false,
   },
   {
     nombre: "Anakin",
     nota:4,
     aprobado: false,
+    libre: false,
+    promocion: false,
   },
   {
     nombre: "Yoda",
     nota: 10,
     aprobado: true,
+    libre: true,
+    promocion: false,
+
+  },
+  {
+    nombre: "Luke Skywalker",
+    nota: 7,
+    aprobado: true,
+    libre: false,
+    promocion: true,
+
+  },
+  {
+    nombre: "Leia Organa",
+    nota: 9,
+    aprobado: true,
+    libre: false,
+    promocion: true,
+  },
+  {
+    nombre: "Han Solo",
+    nota: 6,
+    aprobado: false,
+    libre: true,
+    promocion: false,
+
+  },
+  {
+    nombre: "Padmé Amidala",
+    nota: 8,
+    aprobado: true,
+    libre: true,
+    promocion: false,
   }
-  ]
+]
+
 }
